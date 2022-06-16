a881d1904dab2f49f04d7685a59c38612d22d863
CharlieLeee <lichengkun0805@gmail.com>
2022-06-17 01:42:44 +0200

    clean a bit the repo; add notebook

--- izar_scripts/batch2.sh
+++ izar_scripts/batch2.sh
@@ -1,7 +1,7 @@
-python torch_official_train.py --batch_size 16 --seed 40 --epochs 100 --baseoptim 'sgd' --secoptim 'sam' --model 'resnet18' --norm_type 'normalize' &
-python torch_official_train.py --batch_size 64 --seed 40 --epochs 100 --baseoptim 'sgd' --secoptim 'sam' --model 'resnet18' --norm_type 'normalize' 
-python torch_official_train.py --batch_size 128 --seed 40 --epochs 100 --baseoptim 'sgd' --secoptim 'sam' --model 'resnet18' --norm_type 'normalize' &
+python run.py --batch_size 16 --seed 40 --epochs 100 --baseoptim 'sgd' --secoptim 'sam' --model 'resnet18' --norm_type 'normalize' &
+python run.py --batch_size 64 --seed 40 --epochs 100 --baseoptim 'sgd' --secoptim 'sam' --model 'resnet18' --norm_type 'normalize' 
+python run.py --batch_size 128 --seed 40 --epochs 100 --baseoptim 'sgd' --secoptim 'sam' --model 'resnet18' --norm_type 'normalize' &
 
-python torch_official_train.py --batch_size 16 --seed 40 --epochs 100 --baseoptim 'sgd' --secoptim 'none' --model 'resnet18' --norm_type 'normalize' 
-python torch_official_train.py --batch_size 64 --seed 40 --epochs 100 --baseoptim 'sgd' --secoptim 'none' --model 'resnet18' --norm_type 'normalize' &
-python torch_official_train.py --batch_size 128 --seed 40 --epochs 100 --baseoptim 'sgd' --secoptim 'none' --model 'resnet18' --norm_type 'normalize' 
+python run.py --batch_size 16 --seed 40 --epochs 100 --baseoptim 'sgd' --secoptim 'none' --model 'resnet18' --norm_type 'normalize' 
+python run.py --batch_size 64 --seed 40 --epochs 100 --baseoptim 'sgd' --secoptim 'none' --model 'resnet18' --norm_type 'normalize' &
+python run.py --batch_size 128 --seed 40 --epochs 100 --baseoptim 'sgd' --secoptim 'none' --model 'resnet18' --norm_type 'normalize' 
